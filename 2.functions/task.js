7183aa9c7df0b338e8d927b5eebcc37aa0cc4714
lissum <151545906+lissum@users.noreply.github.com>
2024-11-12 20:49:58 +0500

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,70 @@
 function getArrayParams(...arr) {
+	let max = Math.max(...arr);
+	let min = Math.min(...arr);
 
-  return { min: min, max: max, avg: avg };
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+
+	let avg = Number((sum / arr.length).toFixed(2));
+
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
 function summElementsWorker(...arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
 
+	return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
 
+	return Math.max(...arr) - Math.min(...arr);
 }
 
 function differenceEvenOddWorker(...arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
 
+	let sumEven = arr.filter(num => num % 2 === 0).reduce((acc, curr) => acc + curr, 0);
+	let sumOdd = arr.filter(num => num % 2 !== 0).reduce((acc, curr) => acc + curr, 0);
+	return sumEven - sumOdd;
 }
 
 function averageEvenElementsWorker(...arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
 
+	let evenElements = arr.filter(num => num % 2 === 0);
+	if (evenElements.length === 0) {
+		return 0;
+	}
+
+	let sumEven = evenElements.reduce((acc, curr) => acc + curr, 0);
+	return Number((sumEven / evenElements.length).toFixed(2));
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
 
+	for (let i = 0; i < arrOfArr.length; i++) {
+		let workerResult = func(...arrOfArr[i]);
+		if (workerResult > maxWorkerResult) {
+			maxWorkerResult = workerResult;
+		}
+	}
+
+	return maxWorkerResult;
 }
