fe840294417bbe1e0d679477786c7c1dd3d28c80
lissum <151545906+lissum@users.noreply.github.com>
2024-11-13 08:03:28 +0500

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function(...marksToAdd) {
+	this.marks.push(...marksToAdd);
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.getAverage = function() {
+	if (this.marks.length === 0) {
+		return 0;
+	}
 
-Student.prototype.exclude = function (reason) {
-  
-}
+	let sum = this.marks.reduce((accumulator, current) => accumulator + current, 0);
+
+	return sum / this.marks.length;
+};
+
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+
+	this.excluded = reason;
+};
