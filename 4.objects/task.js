b24fa2f8c784f6817455f7b48e8078c242e50189
lissum <151545906+lissum@users.noreply.github.com>
2024-11-13 08:06:42 +0500

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,6 +10,9 @@ Student.prototype.setSubject = function(subjectName) {
 };
 
 Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.hasOwnProperty('marks')) {
+		return;
+	}
 	this.marks.push(...marksToAdd);
 };
 
